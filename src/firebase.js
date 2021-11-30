13a3e3be43b9fc8a931962c1aa1ba48b362562c6
Oluwamayowa George <themayowageorge@gmail.com>
2021-11-30 16:45:01 +0100

    Hidden Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlN0Gk9oUGmsBKDBSoNnSr8v-u19hM6a4",
-    authDomain: "recreate-5f12c.firebaseapp.com",
-    projectId: "recreate-5f12c",
-    storageBucket: "recreate-5f12c.appspot.com",
-    messagingSenderId: "526526843793",
-    appId: "1:526526843793:web:646ddffc3f05a17e522830",
-    measurementId: "G-D9LG2N5NHG"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PORJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
